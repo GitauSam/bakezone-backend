b5ca01dd30330f6bbdbf3187cc573637385de39f
anon <apple@APPLEs-MacBook-Air.local>
2021-08-12 14:06:14 +0300

    adds logs for debugging in pastry api controller

--- app/Http/Controllers/pastry/PastryApiController.php
+++ app/Http/Controllers/pastry/PastryApiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\pastry;
 use App\Http\Controllers\Controller;
 use App\Models\pastry\Pastry;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use PhpParser\Node\Stmt\Catch_;
 
 class PastryApiController extends Controller
 {
@@ -12,6 +14,26 @@ class PastryApiController extends Controller
 
     public function getAllActivePastries() 
     {
-        return response(Pastry::all(), 200, []);
+        $pastries = null;
+
+        try {
+
+            Log::debug("Fetching pastries");
+            
+            $pastries = response(Pastry::all(), 200, []);
+
+            Log::debug(json_encode($pastries));
+
+            Log::debug("Completed fetching pastries");
+            
+            return $pastries;
+                
+        } catch(\Exception $e) {
+            Log::debug("Exception occurred");
+            Log::debug("Exception message: " . $e->getMessage());
+            Log::debug(json_encode($e));
+        }
+
+        return $pastries;
     }
 }
